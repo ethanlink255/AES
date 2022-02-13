9c25fd33bb3832f46f22ed8c81b05ab4a20097f0
Ethan Link <ethanlink255@gmail.com>
2022-02-13 01:40:28 -0500

    Finished keyscheduler

--- keyscheduler.h
+++ keyscheduler.h
@@ -1,1 +1,9 @@
-char* keyscheduler(unsigned char ikey[16]);
+#include "stdio.h"
+#include "stdint.h"
+#include <string.h>
+#include <stdlib.h>
+
+uint8_t* keyscheduler(uint8_t ikey[16]);
+uint32_t bytemerge(uint8_t b0, uint8_t b1, uint8_t b2, uint8_t b3);
+uint32_t g(uint32_t w, uint8_t rc);
+uint32_t subbytes(uint32_t w);
